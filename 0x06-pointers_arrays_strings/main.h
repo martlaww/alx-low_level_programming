e0eca8ea44f577f3988ec21c04261108078a30d2
martlaww <martlaww135@gmail.com>
2023-03-05 22:05:32 -0800

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
-MAIN.H file
-
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -9,14 +7,14 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *a);
-char *cap_string(char *a);
+char *string_toupper(char *);
+char *cap_string(char *);
 char *leet(char *a);
 char *rot13(char *a);
 void print_number(int n);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
 #endif
-
 
